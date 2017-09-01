24523315ad59d064b331dcf809f225079bdc4629
youthfighter <youthfighter@163.com>
2017-09-01 21:33:37 +0800

    1.完成所有书信息的获取

--- settings/bookList.js
+++ settings/bookList.js
@@ -1,36 +1,20 @@
 /**
  * Created by Administrator on 2017/8/16 0016.
  */
-/*
 module.exports = {
     url: 'http://www.biquku.com/xiaoshuodaquan/',
     encode: 'gbk',
     content: {
-        'selector': '#main .novellist',
-        'children': {
-            'bookIndex': {
-                'selector': 'h2',
-                'value': ['text']
-            },
-            'bookList': {
-                'selector': 'ul li a',
-                // 'eq': 0,
-                'value': {
+        'selector': '#main',
+        'children':{
+            'bookList':{
+                'selector':'.novellist ul li a',
+                'value':{
                     'name': ['text'],
                     'url': ['attr', 'href'],
                 }
             }
         }
-    }
-}*/
-module.exports = {
-    url: 'http://www.biquku.com/xiaoshuodaquan/',
-    encode: 'gbk',
-    content: {
-        'selector': '#main .novellist ul li a',
-        'value':{
-            'name': ['text'],
-            'url': ['attr', 'href'],
-        }
+
     }
 }
--- src/start.js
+++ src/start.js
@@ -10,32 +10,45 @@ const BookSpider = require('./controller/BookSpider');
 const ChapterSpider = require('./controller/ChapterSpider');
 const Async = require('./tools/promise-async');
 const chapterListConfig = require('../settings/chapterList');
-
-const url = 'http://www.37zw.net/0/613/';
-chapterListConfig.url = url;
-let chapterList;
-new spiderRequest(chapterListConfig).getContent()
-    .then(result=>{
-        chapterList = result.chapterList;
-        chapterList.map(value=>{
-            value.url = result.url + value.url;
-            return value;
-        });
-        return new Book(result.bookinfo.name,result.bookinfo.author).save();
+const bookListConfig = require('../settings/bookList');
+/*new spiderRequest(bookListConfig).getContent()
+    .then(data=>{
+        return Async.eachLimit(data.bookList,fun2,1);
     })
-    .then(bookId=>{
-        chapterList.map((value,index)=>{
-            value.bookId = bookId;
-            return value;
-        });
-        return Async.eachLimit(chapterList,fun,10)
-    })
-    .then(result=>{
-        console.log(result);
+    .then(data=>{
+        console.log('本次更新完成!');
     })
     .catch(err=>{
-        console.log(err);
-    })
+        console.log('更新失败');
+    })*/
+
+const url = 'http://www.37zw.net/0/613/';
+fun2({url:'http://www.37zw.net/0/613/'});
+function fun2(value,index) {
+    chapterListConfig.url = value.url;
+    let chapterList;
+    return new spiderRequest(chapterListConfig).getContent()
+        .then(result=>{
+            chapterList = result.chapterList;
+            chapterList.map(value=>{
+                value.url = result.url + value.url;
+                return value;
+            });
+            return new Book(result.bookinfo.name,result.bookinfo.author).save();
+        })
+        .then(bookId=>{
+            chapterList.map((value,index)=>{
+                value.bookId = bookId;
+                return value;
+            });
+            Async.eachLimit(chapterList,fun,10);
+            return bookId;
+        })
+        .then(bookId=>{
+            // console.log(`更新${bookId}成功!`)
+        })
+}
+
 
 function fun(value,index) {
     return new ChapterSpider(value.url).getContent()
@@ -43,6 +56,7 @@ function fun(value,index) {
             return new Chapter(value.bookId,index+1,value.name,content).save();
         })
         .then((chapterId)=>{
+        //console.log(`更新${value.name}成功!`);
             return chapterId;
         })
         .catch(err=>{
@@ -50,6 +64,7 @@ function fun(value,index) {
             return;
         })
 }
+
 
 
 
